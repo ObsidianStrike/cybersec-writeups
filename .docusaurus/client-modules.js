ca3e306da6f55b9b7b91a6173f771690bd7295dc
user <user@example.com>
2025-04-20 23:03:54 -0500

    Deploy website - based on 821568828350b981e9fe328e0316f7e6c3806af9

--- .docusaurus/client-modules.js
+++
@@ -1,6 +0,0 @@
-export default [
-  require("/home/user/cybersec-writeups/node_modules/infima/dist/css/default/default.css"),
-  require("/home/user/cybersec-writeups/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
-  require("/home/user/cybersec-writeups/node_modules/@docusaurus/theme-classic/lib/nprogress"),
-  require("/home/user/cybersec-writeups/src/css/custom.css"),
-];
