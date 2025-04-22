975127aa78fa7600d4acaba45860068b695c6a11
user <user@example.com>
2025-04-22 15:39:52 -0500

    new file:   static/CNAME

--- .docusaurus/docusaurus.config.mjs
+++ .docusaurus/docusaurus.config.mjs
@@ -4,7 +4,7 @@
  * Modify the docusaurus.config.js file at your site's root instead.
  */
 export default {
-  "title": "ObsidianStrike CyberSec Writeups",
+  "title": "ObsidianStrike Pentesting Writeups",
   "favicon": "img/favicon.ico",
   "url": "https://docs.obsidianstrike.com",
   "baseUrl": "/",
