5a21979e359a0e32d8a23802d135c278b8cc604a
user <user@example.com>
2025-04-22 14:38:08 -0500

    modified:   docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -3,7 +3,7 @@ import {themes as prismThemes} from 'prism-react-renderer';
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'ObsidianStrike CyberSec Writeups',
+  title: 'ObsidianStrike Pentesting Writeups',
   //tagline: 'ObsidianStrike Cybersecurity Writeups',
   favicon: 'img/favicon.ico',
 
