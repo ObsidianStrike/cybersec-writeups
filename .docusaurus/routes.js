786db0936d949b3b016163382c2f9ea8cbc85bdc
user <user@example.com>
2025-04-23 18:44:41 -0500

    deleted:    docs/how-to/Active Recon/intro.md 	deleted:    docs/how-to/Passive Recon/intro.md

--- .docusaurus/routes.js
+++ .docusaurus/routes.js
@@ -3,17 +3,52 @@ import ComponentCreator from '@docusaurus/ComponentCreator';
 
 export default [
   {
+    path: '/__docusaurus/debug',
+    component: ComponentCreator('/__docusaurus/debug', '5ff'),
+    exact: true
+  },
+  {
+    path: '/__docusaurus/debug/config',
+    component: ComponentCreator('/__docusaurus/debug/config', '5ba'),
+    exact: true
+  },
+  {
+    path: '/__docusaurus/debug/content',
+    component: ComponentCreator('/__docusaurus/debug/content', 'a2b'),
+    exact: true
+  },
+  {
+    path: '/__docusaurus/debug/globalData',
+    component: ComponentCreator('/__docusaurus/debug/globalData', 'c3c'),
+    exact: true
+  },
+  {
+    path: '/__docusaurus/debug/metadata',
+    component: ComponentCreator('/__docusaurus/debug/metadata', '156'),
+    exact: true
+  },
+  {
+    path: '/__docusaurus/debug/registry',
+    component: ComponentCreator('/__docusaurus/debug/registry', '88c'),
+    exact: true
+  },
+  {
+    path: '/__docusaurus/debug/routes',
+    component: ComponentCreator('/__docusaurus/debug/routes', '000'),
+    exact: true
+  },
+  {
     path: '/404',
     component: ComponentCreator('/404', '5c5'),
     exact: true
   },
   {
     path: '/docs',
-    component: ComponentCreator('/docs', '7cd'),
+    component: ComponentCreator('/docs', 'c07'),
     routes: [
       {
         path: '/docs',
-        component: ComponentCreator('/docs', '90d'),
+        component: ComponentCreator('/docs', '926'),
         routes: [
           {
             path: '/docs/tags',
@@ -47,7 +82,7 @@ export default [
           },
           {
             path: '/docs',
-            component: ComponentCreator('/docs', '247'),
+            component: ComponentCreator('/docs', '3f6'),
             routes: [
               {
                 path: '/docs/concept/active-recon',
@@ -116,12 +151,6 @@ export default [
                 sidebar: "tutorialSidebar"
               },
               {
-                path: '/docs/how-to/Active Recon/intro',
-                component: ComponentCreator('/docs/how-to/Active Recon/intro', '714'),
-                exact: true,
-                sidebar: "tutorialSidebar"
-              },
-              {
                 path: '/docs/how-to/OpSec/c2',
                 component: ComponentCreator('/docs/how-to/OpSec/c2', 'f2a'),
                 exact: true,
@@ -130,12 +159,6 @@ export default [
               {
                 path: '/docs/how-to/OpSec/payload-obfuscation',
                 component: ComponentCreator('/docs/how-to/OpSec/payload-obfuscation', '000'),
-                exact: true,
-                sidebar: "tutorialSidebar"
-              },
-              {
-                path: '/docs/how-to/Passive Recon/intro',
-                component: ComponentCreator('/docs/how-to/Passive Recon/intro', '695'),
                 exact: true,
                 sidebar: "tutorialSidebar"
               },
--- docs/how-to/Active Recon/intro.md
+++
@@ -1,8 +0,0 @@
----
-id: intro
-title: Active Recon
-sidebar_label: shhhh
----
-
-This **How‑To** quadrant will walk you step‑by‑step through tasks like subdomain enumeration and directory brute‑forcing.
-
